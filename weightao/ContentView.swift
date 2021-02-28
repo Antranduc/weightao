b62e3dd5bff4daa68ddfbbc73377e17e3e1bbe42
Sheharyar Umar <sumar@wayfair.com>
2021-02-28 14:34:53 -0500

    Initial Commit

---
+++ weightao/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  weightao
+//
+//  Created by Sheharyar Umar on 2/28/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ weightao/weightaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weightaoApp.swift
+//  weightao
+//
+//  Created by Sheharyar Umar on 2/28/21.
+//
+
+import SwiftUI
+
+@main
+struct weightaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
